a6a624654eca97a72ed2e7a2a5e23c1c176c379d
darKKOREO <tanapat.daoloy@mail.kmutt.ac.th>
2022-09-10 17:06:34 +0700

    1st_commti

--- concatArray.js
+++ concatArray.js
@@ -1,6 +1,28 @@
 const { template } = require('@babel/core')
 
 function concatArray(array1, array2) {
-  //code here
+  let arr1 = array1
+  let arr2 = array2
+  
+  if(arr1 === undefined || arr1 === null){
+    if(arr2 != undefined && arr2 != null){
+      return arr2
+    }else{
+      return undefined
+    }
+  }
+  if(arr2 === undefined || arr2 === null){
+    if(arr1 != undefined && arr1 != null){
+      return arr1
+    }else{
+      return undefined
+    }
+  }else{
+    return arr1.concat(arr2);
+  }
+
 }
 module.exports = concatArray
+
+
+
